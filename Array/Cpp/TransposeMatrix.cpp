c152aa3459bbcdb5585804aa074a90b150e1719b
krishnasaw8340 <krishnakrsaw8340@gmail.com>
2023-08-02 23:01:02 +0530

    function-added

--- Array/Cpp/TransposeMatrix.cpp
+++ Array/Cpp/TransposeMatrix.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
-int main() 
+void transpose()
 {
     
+}
+int main() 
+{
+    int arr[5][5] = {
+        {1,1,1,1,1},
+        {2,2,2,2,2},
+        {3,3,3,3,3},
+        {4,4,4,4,4}
+    };
+
     return 0;
 }
